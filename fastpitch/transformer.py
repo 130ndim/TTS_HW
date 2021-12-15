3fc32d5025ad13631442e35cf2fa3aa1046949d4
Dmitry <130ndim@gmail.com>
2021-12-15 20:58:19 +0300

    modifications

--- fastpitch/transformer.py
+++ fastpitch/transformer.py
@@ -194,20 +194,22 @@ class FFTransformer(nn.Module):
                     dropatt=dropatt, pre_lnorm=pre_lnorm)
             )
 
-    def forward(self, dec_inp, seq_lens=None, conditioning=0):
+    def forward(self, dec_inp, seq_lens=None, conditioning=0, proms=None):
+        prom_inp = 0
         if self.word_emb is None:
             inp = dec_inp
             mask = mask_from_lens(seq_lens).unsqueeze(2)
-            prom_inp = 0
         else:
             inp = self.word_emb(dec_inp)
             # [bsz x L x 1]
             mask = (dec_inp != self.padding_idx).unsqueeze(2)
+            if proms is not None:
+                prom_inp = self.prom_emb(proms) * mask
 
         pos_seq = torch.arange(inp.size(1), device=inp.device).to(inp.dtype)
         pos_emb = self.pos_emb(pos_seq) * mask
 
-        out = self.drop(inp + pos_emb + conditioning)
+        out = self.drop(inp + pos_emb + conditioning + prom_inp)
 
         for layer in self.layers:
             out = layer(out, mask=mask)
--- scripts/train.sh
+++ scripts/train.sh
@@ -16,9 +16,9 @@ export OMP_NUM_THREADS=1
 : ${LEARNING_RATE:=0.1}
 
 # Adjust these when the amount of data changes
-: ${EPOCHS:=1000}
-: ${EPOCHS_PER_CHECKPOINT:=10}
-: ${WARMUP_STEPS:=1000}
+: ${EPOCHS:=10}
+: ${EPOCHS_PER_CHECKPOINT:=2}
+: ${WARMUP_STEPS:=300}
 : ${KL_LOSS_WARMUP:=100}
 
 # Train a mixed phoneme/grapheme model
